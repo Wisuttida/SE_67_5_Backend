64cda6f1e86dd87df84dfb3ed1d504ffa90e9914
FirstZadiz <dickcockmoke@gmail.com>
2025-02-16 21:13:21 +0700

    Add spring template

---
+++ src/main/java/com/perfume/beta/ThaiPerfumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.perfume.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiPerfumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiPerfumeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfume/beta/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.perfume.beta;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
