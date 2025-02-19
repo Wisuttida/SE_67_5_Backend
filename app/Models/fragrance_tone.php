cf5c39c435a0c46cb143e70e0982cc2abb980297
FirstZadiz <dickcockmoke@gmail.com>
2025-02-19 21:55:11 +0700

    Create backend using Laravel
    Also create model for all table from database

---
+++ app/Models/fragrance_tone.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class fragrance_tone extends Model
+{
+    use HasFactory;
+}
--- src/test/java/com/perfume/beta/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.perfume.beta;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
