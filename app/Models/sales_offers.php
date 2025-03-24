79818ef3d8b4b26265b08426ce7b3aca70c81bca
Wisuttida <wysuttida@gmail.com>
2025-03-24 13:33:30 +0700

    editcreateoffers

--- app/Models/sales_offers.php
+++ app/Models/sales_offers.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class sales_offers extends Model
 {
     use HasFactory;
+    public function salePost()
+    {
+        return $this->belongsTo(sales_post::class, 'sales_post_post_id', 'post_id');
+    }
 }
